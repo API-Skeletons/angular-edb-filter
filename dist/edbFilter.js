655c9e3da4e932dd592e3cd21de5a7187df4db54
Tom H Anderson <tom.h.anderson@notevault.com>
2017-01-16 23:05:37 -0700

    README

--- dist/edbFilter.js
+++ dist/edbFilter.js
@@ -1,11 +1,12 @@
-angular.directive('edbFilter', function() {
+angular.module('edbFilter', [])
+.directive('edbFilter', function() {
   return {
     restrict: 'E',
     scope: {
       field: "@field",
       alias: "@alias"
     },
-    templateUrl: 'directive/edb-filter.html',
+    templateUrl: 'bower_components/angular-edb-filter/dist/template/edb-filter.html',
     transclude: true,
     compile: function compile(tElement, tAttrs, transclude) {
       return {
